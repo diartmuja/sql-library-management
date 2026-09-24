052e0667e9b03d5449e4c3c6803ebcd8416ab859
diartmuja <diartmuja7@gmail.com>
2026-09-24 13:45:48 +0200

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO books
+(title, author, genre, publication_year, price, stock, rating, language)
+VALUES
+('The Midnight Club', 'Christopher Pike', 'Horror', 2022, 10.40, 2, 4.0, 'English');
