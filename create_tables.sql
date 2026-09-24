497ce6d5f131f4d3b75a2185b77e7c29c37f3992
diartmuja <diartmuja7@gmail.com>
2026-09-24 13:47:01 +0200

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,1 +1,12 @@
+CREATE TABLE books (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    author TEXT NOT NULL,
+    genre TEXT,
+    publication_year INTEGER,
+    price REAL,
+    stock INTEGER,
+    rating REAL,
+    language TEXT
+);
 
